17ff7af6f321a78ef5eafb04d1fbc53103f6af88
mccarta6 <88693925+mccarta6@users.noreply.github.com>
2023-11-10 14:06:43 -0500

    Moved the elements in game.js to the two scene files

--- Scene1.js
+++ Scene1.js
@@ -1,6 +1,22 @@
 class Scene1 extends Phaser.Scene {
-    constructor() {
-      super("bootGame");
+  constructor() {
+    super("bootGame");
+  }
+  
+  preload() {
+      // This method is called once at the beginning
+      // It will load all the assets, like sprites and sounds 
+      this.load.image('background', 'assets/background.png');
+      this.load.spritesheet('player', 'assets/dog.png',{
+        frameWidth: 190.5,
+        frameHeight: 200
+      });
+      this.load.image('food', 'assets/str.png');
     }
-    
+	
+	//Switch to Scene2
+	create() {
+		this.scene.start("playGame");
+	}
+  
 }
--- Scene2.js
+++ Scene2.js
@@ -1,6 +1,62 @@
 class Scene2 extends Phaser.Scene {
-    constructor() {
+  constructor() {
       super("playGame");
-    }
-    
   }
+
+  create() {
+      //Initializes our scene, like the positions of the sprites
+      this.background = this.add.tileSprite(0, 0, this.game.config.width, this.game.config.height, "background");
+      this.background.setOrigin(0, 0);
+
+      this.player = this.physics.add.sprite(500, 450, 'player');
+      this.createAnimations();
+
+      this.food = this.physics.add.sprite(300, 300, 'food');
+      this.food.setScale(0.01);
+
+      this.arrow = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+  //Called every frame and allows for continuous updates
+      this.updateMovement(this.arrow);
+  }
+
+  updateMovement(cursors) {
+
+      //Stop animations by default
+      this.player.anims.stop();
+
+      //Move left
+      if (cursors.left.isDown) {
+          this.player.x -= 3;
+          this.player.anims.play("left", true);
+      }
+
+      //Move right
+      else if (cursors.right.isDown) {
+          this.player.x += 3;
+          this.player.anims.play("right", true);
+      } 
+
+}
+
+  createAnimations() {
+  
+  //Left animation
+      this.anims.create({
+          key: "left",
+          frames: this.anims.generateFrameNumbers("player", { start: 0, end: 1 }),
+          frameRate: 10,
+          repeat: -1,
+      });
+  
+  //Right animation
+      this.anims.create({
+          key: "right",
+          frames: this.anims.generateFrameNumbers("player", { start: 2, end: 3 }),
+          frameRate: 10,
+          repeat: -1,
+      });
+  }
+}
