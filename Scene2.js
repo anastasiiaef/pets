910936bd6f4d0aa2a1de99ac862e5b823f081f92
Anastasiia Efimova <nastkaefff@mail.ru>
2023-11-24 18:27:22 -0500

    Added a food movement.

--- Scene2.js
+++ Scene2.js
@@ -4,23 +4,41 @@ class Scene2 extends Phaser.Scene {
   }
 
   create() {
+
       //Initializes our scene, like the positions of the sprites
       this.background = this.add.tileSprite(0, 0, this.game.config.width, this.game.config.height, "background");
+      //this.background = this.add.image(0, 0, "background");
       this.background.setOrigin(0, 0);
 
       this.player = this.physics.add.sprite(500, 450, 'player');
       this.createAnimations();
 
       this.food = this.physics.add.sprite(300, 300, 'food');
-      this.food.setScale(0.01);
+      this.food.setScale(0.02);
 
       this.arrow = this.input.keyboard.createCursorKeys();
   }
 
-  update() {
-  //Called every frame and allows for continuous updates
-      this.updateMovement(this.arrow);
-  }
+    update() {
+    //Called every frame and allows for continuous updates
+        this.updateMovement(this.arrow);
+        this.moveFood(this.food, 3);
+
+        //this.background.tilePositionX += 0.3;
+    }
+
+    moveFood(food, speed) {
+        food.y += speed;
+        if(food.y > 600) {
+            this.resetFoodPos(food);
+        }
+    }
+
+    resetFoodPos(food) {
+        food.y = 0;
+        var randomX = Phaser.Math.Between(0, 1000);
+        food.x = randomX;
+    }
 
   updateMovement(cursors) {
 
